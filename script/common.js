e62c47901a1523f84e2f86f3ef47709a15257099
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2024-08-27 11:53:58 +0900

    バージョン名を変更。

--- script/common.js
+++ script/common.js
@@ -1,4 +1,4 @@
-console.log("ニコニコ動画クラシックスタイル バージョン1.0.2（仮）\n© 2024 Bymnet1845");
+console.log("ニコニコ動画クラシックスタイル バージョン1.0.3（仮）\n© 2024 Bymnet1845");
 
 chrome.storage.local.get("iconType", (content) => {
 	if (content.iconType !== undefined && content.iconType !== "" && content.iconType !== "circle") {
