c9bfaa51f4ab23ae807b7b7dc9a40e5af7b88774
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2025-04-23 01:34:17 +0900

    バージョンを変更。

--- script/common.js
+++ script/common.js
@@ -1,4 +1,4 @@
-console.log("ニコニコクラシックスタイル バージョン1.3.2（γ）\n© 2024–2025 Bymnet1845");
+console.log("ニコニコクラシックスタイル バージョン1.3.3（γ）\n© 2024–2025 Bymnet1845");
 
 chrome.storage.local.get("iconType", (content) => {
 	if (content.iconType !== "circle") document.body.classList.add("niconico-classic_icon-is-square");
