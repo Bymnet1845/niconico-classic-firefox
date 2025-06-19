9b3d6621b2aead311a83ecf73b82c535de83eb4d
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2025-06-19 23:39:32 +0900

    簡単マイリストのデザインを調整。

--- script/video-common.js
+++ script/video-common.js
@@ -101,7 +101,7 @@ setInterval(() => {
 								case 200:
 									NICONICO_CLASSIC_EASY_MYLIST_ELEMENT.insertAdjacentHTML(
 										"afterbegin",
-										`<div id="niconico-classic_easy-mylist-form"><select></select><button>追加</button></div><div id="niconico-classic_easy-mylist-menu"><button id="niconico-classic_easy-deflist-button" type="button">🕓️ あとで見るに追加</button><button id="niconico-classic_easy-mylist-option-button" type="button">追加オプション</button></div><div id="niconico-classic_easy-mylist-option-form"><label for="niconico-classic_easy-mylist-description">メモ（マイリストコメント）</label><textarea name="niconico-classic_easy-mylist-description"></textarea></div>`
+										`<div id="niconico-classic_easy-mylist-form"><select class="niconico-classic_easy-mylist-tray-action"></select><button>追加</button></div><div id="niconico-classic_easy-mylist-menu"><button id="niconico-classic_easy-deflist-button" type="button">あとで見るに追加</button><button id="niconico-classic_easy-mylist-option-button" class="niconico-classic_easy-mylist-tray-action" type="button">追加オプション</button></div><div id="niconico-classic_easy-mylist-option-form"><label for="niconico-classic_easy-mylist-description">メモ（マイリストコメント）</label><textarea name="niconico-classic_easy-mylist-description"></textarea></div>`
 									);
 
 									const NICONICO_CLASSIC_EASY_MYLIST_SELECT_ELEMENT = document.querySelector(`#niconico-classic_easy-mylist-form select`);
