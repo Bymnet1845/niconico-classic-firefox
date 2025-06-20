1c11e28851cfb81b2c0edbf6d24f123edfe55173
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2025-06-20 17:01:19 +0900

    バージョンのコンソールへの表示処理をトップフレームのみで行う様に。

--- script/common.js
+++ script/common.js
@@ -1,4 +1,4 @@
-console.log("ニコニコクラシックスタイル バージョン1.3.6（γ）\n© 2024–2025 Bymnet1845");
+if (window.top === window) console.log("ニコニコクラシックスタイル バージョン1.3.6（γ）\n© 2024–2025 Bymnet1845");
 
 chrome.storage.local.get("iconType", (content) => {
 	if (content.iconType !== "circle") document.body.classList.add("niconico-classic_icon-is-square");
